6f1db2049098ffaad4b5b7388f20315e6abad9e7
Unumberten <honestgceba@gmail.com>
2024-08-21 10:54:26 +0200

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -14,8 +14,6 @@ const CheckoutForm = ({ totalPrice }) => {
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
 
-  const stripe = useStripe();
-const elements = useElements();
 
 if (!stripe || !elements) {
     console.error("Stripe or Elements not initialized.");
