fb84f5aad1cf53109ae41f5afdab1855c4b86c32
Unumberten <honestgceba@gmail.com>
2024-08-23 09:48:00 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                   <ion-icon name="bag"></ion-icon>
                   <h1>Multimart</h1>
               </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
+              <p>Loem sites when we are the best shop out there.</p>
             </Col>
             <Col md={3} sm={5} className='box'>
               <h2>About Us</h2>
